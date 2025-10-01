04bed3c098fc3a1cf1ccc0664a86fda6fa969e61
Abhishek Singh <singhey2@gmail.com>
2025-10-01 15:09:00 +0530

    chore: fixed ts errors in package

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,5 +1,5 @@
 import { enhance } from '@zenstackhq/runtime';
-import { FetchFn } from '@zenstackhq/tanstack-query/runtime';
+import type { FetchFn } from '@zenstackhq/tanstack-query/runtime';
 import * as models from "@zenstackhq/runtime/models"
 
 export {enhance, type FetchFn, models}
